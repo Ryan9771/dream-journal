aaee87668affd80d5bb02e02b628100232c71066
Ryan <rkp21@ic.ac.uk>
2024-06-20 01:16:10 +0100

    INITIAL COMMIT: Setup react and flask project with cross communication

---
+++ flask-server/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+# Members API Route
+@app.route("/members")
+def members():
+    return jsonify({"members": ["member1", "member2", "member3"]})
+
+@app.route("/")
+def home():
+    return "Hello world"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
