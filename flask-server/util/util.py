5db1b7bc1ed92e865ff5e882d97e1e2b097f972e
Ryan <rkp21@ic.ac.uk>
2024-07-07 23:39:03 +0100

    DELETE: Deleted util in flask

--- flask-server/util/util.py
+++
@@ -1,6 +0,0 @@
-def encrypt_data(data: str, fernet) -> str:
-    return fernet.encrypt(data.encode()).decode()
-
-
-def decrypt_data(encrypted_data: str, fernet) -> str:
-    return fernet.decrypt(encrypted_data.encode()).decode()
